e526e95b883979e35823ebef53a75dad4713b517
Paula⚡ <150956495+prayo-ga@users.noreply.github.com>
2024-06-21 12:07:08 +0200

    Add files via upload

--- Rank02/Level_3/str_capitalizer/str_capitalizer.c
+++ Rank02/Level_3/str_capitalizer/str_capitalizer.c
@@ -1,23 +1,39 @@
 #include <unistd.h>
 
-int	main(int ac, char **av)
+void	str_capitalizer(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	if (ac >= 2)
+	if (str[i] >= 'a' && 'z' >= str[i])
+		str[i] -= 32;
+	write(1, &str[i], 1);
+	while (str[++i])
 	{
-		while (av[1][i])
+		if (str[i] >= 'A' && 'Z' >= str[i])
+			str[i] += 32;
+		if ((str[i] >= 'a' && 'z' >= str[i]) && (str[i - 1] == ' '
+				|| str[i - 1] == '\t'))
+			str[i] -= 32;
+		write(1, &str[i], 1);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	if (argc < 2)
+		write(1, "\n", 1);
+	else
+	{
+		i = 1;
+		while (i < argc)
 		{
-			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-				av[1][i] += 32;
-			if ((av[1][i] >= 'a' && av[1][i] <= 'z') && (av[1][i - 1] == ' '
-					|| av[1][i - 1] == '\t' || av[1][i - 1] == '\0'))
-				av[1][i] -= 32;
-			write(1, &av[1][i], 1);
-			i++;
+			str_capitalizer(argv[i]);
+			write(1, "\n", 1);
+			i += 1;
 		}
 	}
-	write(1, "\n", 1);
 	return (0);
 }
